64865f282dc9127894e7fef23e98b13e360ef690
kasimhusain00 <wp79.department@example.com>
2024-06-24 17:48:36 +0530

    print message

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -2,54 +2,71 @@ import React, { useState } from "react";
 import { Button, Input } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { add, deletee, editt, update } from "../Action/action";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Todo = () => {
   const [change, setChange] = useState({ firstval: "" });
-  const[field, setField] = useState(false);
+  const [field, setField] = useState(false);
   const [editId, setEditId] = useState(null);
 
   const dispatch = useDispatch();
+
   const todos = useSelector((state) => state.tododata.data);
   console.log(todos, "todos");
 
   const handleClick = () => {
-    const newTodo = { id: todos.length + 1, firstval: change.firstval };
-    dispatch(add(newTodo));
-    setChange({ firstval: "" });
+    const trimmedValue = change.firstval.trim();
+    if (trimmedValue === "") {
+      // alert("Insert the value first");
+      toast.warn("Insert the value first");
+
+      
+    } else {
+      const newTodo = { id: todos.length + 1, firstval: change.firstval };
+      dispatch(add(newTodo));
+      setChange({ firstval: "" });
+    }
   };
 
   const handleChange = (e) => {
     setChange({ firstval: e.target.value });
   };
 
-  const handleEdit =(id) => {
-    dispatch(editt(id,change));
+  const handleEdit = (id) => {
+    dispatch(editt(id, change));
     let newEditItem = todos.find((data) => {
       // console.log(data);
       return data.id === id;
-    }); 
+    });
     setChange({ firstval: newEditItem.firstval });
     setField(() => !field);
     setEditId(id);
-  }
+  };
   const handleupdate = (id) => {
-    dispatch(update(id,change))
+    dispatch(update(id, change));
     setField(false);
     setEditId(null);
     setChange({ firstval: "" });
-  }
+  };
 
   return (
+    <>
+      <ToastContainer /> 
     <div>
-      <h1 style={{
+      <h1
+        style={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           marginTop: "10%",
           alignItems: "center",
-          color:"whitesmoke"
-        }}>Todo List</h1>
-      <section style={{
+        }}
+      >
+        Todo List
+      </h1>
+      <section
+        style={{
           display: "flex",
           flexDirection: "row",
           justifyContent: "center",
@@ -63,7 +80,10 @@ export const Todo = () => {
           onChange={handleChange}
         />
         &nbsp;&nbsp;&nbsp;
-        <Button onClick={handleClick} style={{ border: "1px solid",backgroundColor:"black" }}>
+        <Button
+          onClick={() => handleClick()}
+          style={{ border: "1px solid", backgroundColor: "black" }}
+        >
           +
         </Button>
       </section>
@@ -96,27 +116,44 @@ export const Todo = () => {
                   <td style={{ width: "15rem" }}>{todo.firstval}</td>
                 )
               } */}
-              <td style={{ width: "15rem",color:"white" }}>{todo.firstval}</td>
+                <td style={{ width: "15rem" }}>{todo.firstval}</td>
                 <td style={{ width: "3rem" }}>
-                  <Button onClick={() => dispatch(deletee(todo.id))}>
+                  <Button
+                    variant="contained"
+                    color="error"
+                    onClick={() => dispatch(deletee(todo.id))}
+                  >
                     Delete
                   </Button>
                 </td>
-                {field && editId === todo.id ? 
-                
-                <td style={{ width: "3rem" }}>
-                  <Button onClick={() => handleupdate(todo.id)}>Update</Button>
-                </td>
-                :
-                <td style={{ width: "3rem" }}>
-                  <Button onClick={() => handleEdit(todo.id)}>Edit</Button>
-                </td>
-              }
+                {field && editId === todo.id ? (
+                  <td style={{ width: "3rem" }}>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      onClick={() => handleupdate(todo.id)}
+                    >
+                      Update
+                    </Button>
+                  </td>
+                ) : (
+                  <td style={{ width: "3rem" }}>
+                    <Button
+                      variant="contained"
+                      color="success"
+                      onClick={() => handleEdit(todo.id)}
+                    >
+                      Edit
+                    </Button>
+                  </td>
+                )}
               </tr>
             </table>
           ))}
         </ul>
       </section>
     </div>
+    </>
+
   );
 };
